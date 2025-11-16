3bd2f59126a9d87e09117d2487201fe2535bebf5
fauzinoorsyabani <227007042@student.unsil.ac.id>
2025-11-17 06:05:56 +0700

    feat: upgrade HowItWorks section with a premium ui

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from "react";
 import Navbar from "./components/layout/Navbar";
 import Hero from "./components/sections/Hero";
-import Features from "./components/sections/Features";
-import HowItWorks from "./components/sections/HowItWorks";
 import Pricing from "./components/sections/Pricing";
 import CTA from "./components/sections/CTA";
 import "./App.css";
 import BackgroundPattern from "./components/layout/BackgrounPatter"
 import Marquee from "./components/ui/Marquee";
 import Partners from "./components/sections/Partners";
+import HowItWorks from "./components/sections/HowItWorks";
 import SimplePricing from "./components/sections/SimplePricing";
 import Footer3 from "./components/layout/Footer3";
-import FloatingParticle from "./components/nextbunny/cta/FloatingParticle";
-import StatCard from "./components/nextbunny/StatCard";
-import Waitlist from "./components/nextbunny/waitlist";
 import CTAWithVortex from "./components/sections/CTAWithVortex";
+
+
 
 export default function App(){
 return (
@@ -22,7 +20,6 @@ return (
     <Navbar />
     <Hero />
     <Partners/>
-    <Features />
     <HowItWorks/>
     <Pricing/>
     <CTAWithVortex/>
